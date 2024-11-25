962ea242ff6e16ce042e77d42b309d664dd0550e
Subarna Khadka <subarna31k@gmail.com>
2024-11-25 20:10:04 +0545

    add .env.example

--- app/api/auth/refresh/route.ts
+++ app/api/auth/refresh/route.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { withRefreshAuth } from "@/lib/jwtRefreshProtected";
 import { NextRequest, NextResponse } from "next/server";
